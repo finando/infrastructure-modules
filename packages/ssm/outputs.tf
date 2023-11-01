beae6edca19c64ef9935e450697163e1675fbaa1
Alexander Zakharov <alexander.zakharov@finando.app>
2023-11-01 12:04:12 +0100

    fix(ssm): add SSM parameter name to output

--- packages/ssm/outputs.tf
+++ packages/ssm/outputs.tf
@@ -2,3 +2,8 @@ output "ssm_parameter_arn" {
   description = "SSM parameter ARN"
   value       = { for key, value in nonsensitive(local.ssm_parameters) : key => aws_ssm_parameter.this[key].arn }
 }
+
+output "ssm_parameter_name" {
+  description = "SSM parameter name"
+  value       = { for key, value in nonsensitive(local.ssm_parameters) : key => aws_ssm_parameter.this[key].name }
+}
